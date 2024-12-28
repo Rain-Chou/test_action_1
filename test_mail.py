e7670a535d83657feed29a7b40032445506704d5
Rain <csmu05@gmail.com>
2024-12-28 16:29:42 +0800

    V7

--- test_mail.py
+++ test_mail.py
@@ -4,25 +4,20 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 sender = 'csmu05@gmail.com'
-receiver = ['csmu2015@gmail.com','csmu05@gmail.com']
+receiver = ['csmu2015@hotmail.com','csmu05@gmail.com']
 passwd = "bplu azwc whbd zrmx"
-
-
-msg = MIMEMultipart()
-msg["From"] = sender
-msg["To"] = receiver
-msg["Subject"] = Header("Test send email","utf-8").encode()
-
-body = "Test send email"
-
-msg_text=MIMEText(body)
-msg.attach(msg_text)
-c = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com",465,context=c) as server:
-    server.login(sender,passwd)
-    server.sendmail(sender,receiver,msg.as_string())
-print("success send mail!")
-
-for msg in receiver:
-    print(msg)
+for i in receiver:
+    msg = MIMEMultipart()
+    msg["From"] = sender
+    msg["To"] = i
+    msg["Subject"] = Header("Test send email","utf-8").encode()
+    
+    body = "Test send email\nhow are you?"
+    msg_text=MIMEText(body)
+    msg.attach(msg_text)
+    c = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL("smtp.gmail.com",465,context=c) as server:
+        server.login(sender,passwd)
+        server.sendmail(sender,i,msg.as_string())
+    print("success send mail!")
